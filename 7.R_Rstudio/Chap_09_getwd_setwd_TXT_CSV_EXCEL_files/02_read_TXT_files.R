4f5d968d25d3c93b71718c37b63d45083fb8a45c
Longistamina <dophamthelong03@gmail.com>
2025-06-07 17:50:30 +0700

    added file chapter 09 6/7/25 17:50

--- 7.R_Rstudio/Chap_09_getwd_setwd_TXT_CSV_EXCEL_files/02_read_TXT_files.R
+++ 7.R_Rstudio/Chap_09_getwd_setwd_TXT_CSV_EXCEL_files/02_read_TXT_files.R
@@ -1,7 +1,12 @@
+data_path <- "data_chap_09/demo_data/"
+
 #-------------------------------------------------------------------#
 #---------------------- use readLines() ----------------------------#
 #-------------------------------------------------------------------#
-content_1 <- readLines("data_chap_09/demo_data/license.txt")
+
+# readLines() return a list/vector contain all lines/elements of the text
+
+content_1 <- readLines(paste(data_path, "license.txt", sep = ""))
 print(content_1)
 # [1] "These data files are derived by splitting up the data set downloaded from:"
 # [2] "https://www.ontario.ca/data/ontario-public-schools-enrolment"              
@@ -14,7 +19,7 @@ print(length(content_1)) # Count the number of lines (or vectors) of this text
 print(nchar(content_1)) # Count the number of characters (elements) of each line (vector) from this text
                         # 74 60 44 59
 
-content_2 <- readLines("data_chap_09/demo_data/bai_tho.txt", encoding = "UTF-8") #For vietnamese
+content_2 <- readLines(paste(data_path, "license.txt", sep = ""), encoding = "UTF-8") #For vietnamese
 print(content_2)
 # [1] "Mùa xuân là Tết trồng cây"           
 # [2] "Làm cho đất nước càng ngày càng xuân"
@@ -36,9 +41,62 @@ print(file.size("data_chap_09/demo_data/bai_tho.txt")) # 89 (means 89 bytes)
 # default, scan() treats inputs as real
 # so, to scan text file, must set what = character()
 
-lst_content_2 <- scan("data_chap_09/demo_data/bai_tho.txt", encoding = "UTF-8", sep = " ", what = character())
+lst_content_2 <- scan(paste(data_path, "bai_tho.txt", sep = ""), encoding = "UTF-8", sep = " ", what = character())
 print(lst_content_2)
 print(length(lst_content_2))
 # [1] "Mùa"   "xuân"  "là"    "Tết"   "trồng" "cây"   "Làm"   "cho"   "đất"  
 # [10] "nước"  "càng"  "ngày"  "càng"  "xuân" 
 # 14 elements (words)
+
+
+#--------------------------------------------------------------------#
+#---------------------- use read.table() ----------------------------#
+#--------------------------------------------------------------------#
+
+# read.table() helps read table-type txt file and returns a DATAFRAME
+
+txt_table <- read.table(paste(data_path, "Table0.txt", sep = ""))
+print(txt_table)
+print(class(txt_table)) #"data.frame"
+
+
+#------------------------------------------#
+#------------- use write() ----------------#
+#------------------------------------------#
+
+# write() helps write a bunch of contents into a text file
+
+# use file() to open a file
+content_to_write = "Odin the Alfather"
+
+file_name <- file(
+    description = paste(data_path, "written_text.txt", sep = ""), #define a path to .txt file to open and write
+    open = "a", #open mode is append
+    encoding = "UTF-8"
+)
+
+write(content_to_write, file = file_name) # Write the content_to_write to the writtent_text.txt
+                                          # find and open the writtent_text.txt to check
+
+
+#------------------------------------------#
+#------------- use write() ----------------#
+#------------------------------------------#
+
+# write.table() helps write table-type text into .csv or .tsv (NOT support .xlsx or excel files)
+
+df_to_write = data.frame(
+    "ID" = c(1, 2, 3),
+    "Name" = c("Sensan", "Normadic", "Linking"),
+    "Age" = c(18.2, 14.3, 12.1)
+)
+
+write.table(
+    df_to_write,
+    file = paste(data_path, "written_table.txt", sep = ""), # or .csv or .tsv
+    sep = "\t",
+    row.names = FALSE
+)
+
+# row.names = FALSE to avoid writing row names as a separate column.
+# quote = TRUE to enclose all character or factor data in double quotes (" ")
