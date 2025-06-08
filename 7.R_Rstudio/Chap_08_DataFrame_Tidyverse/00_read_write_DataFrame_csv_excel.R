7d0e6434f732851eeaf8a859c8fdde9c58596476
Longistamina <dophamthelong03@gmail.com>
2025-06-08 22:15:15 +0700

    modified chapter 9 and prepare for chapter 10

--- 7.R_Rstudio/Chap_08_DataFrame_Tidyverse/00_read_write_DataFrame_csv_excel.R
+++ 7.R_Rstudio/Chap_08_DataFrame_Tidyverse/00_read_write_DataFrame_csv_excel.R
@@ -16,7 +16,12 @@ library(readxl) #This has read_excel() to read dataframe from .xlsx or .xls file
 df_csv <- read_csv("data_chap_08/baseball.csv")
 print(df_csv)
 
-df_excel <- read_excel("data_chap_08/Iris.xls")
+df_excel <- read_xlsx(
+    "data_chap_08/Iris.xls", 
+    sheet = 1, # Read the first sheet, or sheet = "sheet_name"
+    skip = 0 # skip no row
+) 
+
 print(df_excel)
 
 # All read commands for csv-type files:
@@ -39,3 +44,29 @@ print(df_excel)
 print(is.data.frame(df_csv))
 
 print(is.data.frame(df_excel))
+
+
+#---------------------------------------------------------#
+#------------ write_csv() and write_xlsx() ---------------#
+#---------------------------------------------------------#
+
+# Below are the functions that helps write your dataframe into a .csv, .tsv or .xlsx files for storing
+
+# write_csv() for .csv files
+write_csv(
+    df_csv, 
+    file = "data_chap_08/written_df.csv", 
+)
+
+# write_tsv() for .tsv files
+write_tsv(
+    df_csv, 
+    file = "data_chap_08/written_df.tsv", 
+)
+
+# write_xlsx() for excel files
+library(writexl)
+write_xlsx(
+    df_excel,
+    path = "data_chap_08/written_df.xlsx"
+)
--- 7.R_Rstudio/Chap_09_getwd_setwd_read_write_TXT_CSV_EXCEL_files/04_read_write_EXCEL_files.R
+++ 7.R_Rstudio/Chap_09_getwd_setwd_read_write_TXT_CSV_EXCEL_files/04_read_write_EXCEL_files.R
@@ -1,43 +1,53 @@
+# install.packages("xlsx")
+# conda install -c conda-forge r-xlsx
+
 #-------------------------------------------------#
-#----------------- read_xlsx() -------------------#
+#----------------- read.xlsx() -------------------#
 #-------------------------------------------------#
 
-# install package "readxl" to read excel files
-# install.packages("readxl")
+library(xlsx)
 
-library(readxl)
-
-emp_xlsx = read_xlsx(
-    path = "data_chap_09/demo_data/emp_data.xlsx",
-    sheet = "emp", #read from sheet "emp", or "sheet = 1" to read the first sheet
-    skip = 0 # skip rows = 0
+emp_xlsx = read.xlsx(
+    file = "data_chap_09/demo_data/emp_data.xlsx",
+    sheetName = "emp", #read from sheet "emp", or "sheet = 1" to read the first sheet
+    startRow = NULL,
+    endRow = NULL
 )
 print(emp_xlsx)
 
 
-city_xlsx = read_xlsx(
-    path = "data_chap_09/demo_data/emp_data.xlsx",
-    sheet = 2, #read from the 2nd sheet, or "city" sheet
-    skip = 0 # skip rows = 0
+city_xlsx = read.xlsx(
+    file = "data_chap_09/demo_data/emp_data.xlsx",
+    sheetIndex = 2, #read from the 2nd sheet, or "city" sheet
+    startRow = NULL,
+    endRow = NULL
 )
 print(city_xlsx)
 
 
 #--------------------------------------------------#
-#----------------- write_xlsx() -------------------#
+#----------------- write.xlsx() -------------------#
 #--------------------------------------------------#
 
+library(tidyverse)
 medals_gold <- read.csv("data_chap_09/demo_data/medals.csv") %>%
     filter(Medal == "Gold")
 head(medals_gold)
 
-# install package "writexl" to write excel files
-# install.packages("writexl")
-# conda install -c conda-forge r-writexl (do this in venv terminal)
+######
+library(xlsx)
 
-library(writexl)
-
-write_xlsx(
+# write into a new .xlsx file
+write.xlsx(
     medals_gold,
-    path = "data_chap_09/demo_data/medals_gold.xlsx"
+    file = "data_chap_09/demo_data/medals_gold.xlsx"
 )
+
+# write into an existed .xlsx file, define the sheet name
+write.xlsx(
+    medals_gold,
+    file = "data_chap_09/demo_data/medals_new.xlsx",
+    sheetName = "Gold",
+    append = TRUE # This helps avoid overwriting the existed sheet 1
+)
+# check the medals_new.xlsx sheet "Gold" to see the result
