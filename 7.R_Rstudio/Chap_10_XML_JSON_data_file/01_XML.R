a44df03c682bd851f1e2d68a77e770d8533ed03b
Longistamina <dophamthelong03@gmail.com>
2025-06-08 23:16:39 +0700

    added chapter 10

---
+++ 7.R_Rstudio/Chap_10_XML_JSON_data_file/01_XML.R
@@ -0,0 +1,3 @@
+# XML = eXtensible Markup Language
+# It is designed for facilitating data transfering and storing
+# Main purpose: simplify the data sharing process between systems, especially via Internet
